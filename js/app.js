0377e710954702d217f2f5360934b577e94bdc76
liyanyan19940914 <921258351@qq.com>
2016-08-05 09:57:08 +0800

    add element

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 const App = React.createClass({
     getInitialState:function () {
         return {
-            isEditor:true
+            isEditor:true,
+            elements:[]
         }
     },
     toggle:function () {
         this.setState({isEditor:!this.state.isEditor})
     },
+    addElement:function (element) {
+        const elements=this.state.elements;
+        elements.push(element);
+        this.setState({elements})
+    },
     render: function () {
         return <div>
             <button onClick={this.toggle}>{this.state.isEditor?'preview':'edit'}</button>
             <div className={this.state.isEditor?'':'hidden'}>
-                <Editor/>
+                <Editor onAdd={this.addElement}/>
             </div>
             <div className={this.state.isEditor?'hidden':''}>
                 <Preview/>
@@ -22,7 +28,20 @@ const App = React.createClass({
 const Editor = React.createClass({
     render: function () {
         return <div>
-            <h1>hello world</h1>
+            <Right onAdd={this.props.onAdd}/>
+        </div>;
+    }
+});
+const Right = React.createClass({
+    add:function () {
+        const element=$('input[name=element]:checked').val();
+        this.props.onAdd(element)
+    },
+    render: function () {
+        return <div>
+            <input type="radio" name="element" value='text'/>文本框
+            <input type="radio" name="element" value='date'/>日期
+            <button onClick={this.add}>+</button>
         </div>;
     }
 });
